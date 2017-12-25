435c62bcd07256f94e1ea9dace2f0df90fa35c87
raineer <delaritaraineer81@gmail.com>
2017-12-25 20:00:02 +0800

    Installing typescript

--- app/app.ts
+++ app/app.ts
@@ -1,19 +1,36 @@
 function shopNow() {
-	// starting a new game
-	
-	let customerName: string = 'John';
-	logCustomer(customerName);
+    // starting a new game
+    
+    let customerName: string | undefined = getInputValue('customername');
+    logCustomer(customerName);
 
-  let messagesElement: HTMLElement | null = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to Eos! Show now....';
- console.log(messagesElement);
+totalCost(80);
+
+}
+function logCustomer(name: string = 'Eos Customer'): void {
+    console.log(`New grocery list for customer: ${name}`);
+}
+
+function totalCost(score: number,  customerName: string = 'Eos Customer'): void {
+ let scoreElement: HTMLElement | null = document.getElementById('totalCost');
+  scoreElement!.innerText = `${score} - ${customerName}`;
+}
+
+function getInputValue(elementID: string): string | undefined {
+  let inputElement : HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+  if (inputElement.value === ' ') {
+    return undefined;
+  }
+  else {
+    return inputElement.value;
+  }
+  
 
 }
 
 
 
-	function logCustomer(name: string): void {
-		console.log(`New grocery list for customer: ${name}`);
-	}
+  
 
 document.getElementById('shopNow')!.addEventListener('click', shopNow);
